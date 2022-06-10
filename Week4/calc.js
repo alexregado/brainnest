3b20bf4443b66b7b3337a2929f7103e169ce2dc0
alexregado <alexregado@gmail.com>
2022-06-10 13:31:47 +0100

    Calculator Almost Done

---
+++ Week4/calc.js
@@ -0,0 +1,126 @@
+let numbers = document.querySelectorAll(".number");
+let numbers123 = document.querySelectorAll(".number");
+let screen = document.querySelector(".result");
+let operators = document.querySelectorAll(".operator");
+let clear = document.querySelector(".clear");
+let equals = document.querySelector(".equal");
+let del = document.querySelector(".delete");
+let calc;
+let numberOn = [];
+let num = 0;
+
+let separator;
+clear.addEventListener("click", () => {
+    screen.textContent = "";
+    numberOn = [];
+})
+
+
+numbers.forEach(number => {
+    number.addEventListener("click", function(){
+        num = "";
+        num = screen.textContent += this.textContent;
+
+        if(this.textContent == "."  && dot > 1){
+            num = num.substring(0, num.length - 1);
+            dot = 0;
+        }
+    });
+});
+
+/* 
+CRIAR MENSAGÆNS DE ERROS PARA INSERIR UM SIMBOLO APOS O PONTO
+DOIS SIMBOLOS SEGUIDOS
+*/
+
+del.addEventListener("click", ()=>{num = num.substring(0, num.length - 1); screen.textContent = num;})
+
+
+
+operators.forEach(operator => {
+    operator.addEventListener("click", function(){
+        if(num.includes("/0")){
+            alert("U dumb or what?");
+            num = num.substring(0, num.length - 1);
+            num = num.substring(0, num.length - 1);
+            return
+        }
+
+        //CHECKS IF THERE IS ALREADY ANY OPERATOR
+        for(let i = 0; i < operators.length; i++){
+            if(num.includes(operators[i].textContent)){
+                sumMade = num.split(separator)
+                let finalResult = operate(sumMade[0], separator, sumMade[1]); 
+                screen.textContent = finalResult;
+                finalResult = num;
+                console.clear();
+            }
+        }
+
+            num = screen.textContent += this.textContent;
+            separator = this.textContent;
+            oper = this.textContent;
+
+    });
+});
+
+//CHECKS FOR DOTS 
+let dot = 0;
+window.setInterval(function(){
+        if(num.includes(".")){
+            dot++;
+        num = num.replace(".." || "..." || "...." || ".....", ".")
+        screen.textContent = num;
+        console.log('as', num)
+
+        console.log(dot)
+
+
+    }
+
+
+    let lastt = num.substr(-2, 3)
+    console.log(num, lastt)
+
+    
+}, 600);
+
+
+//DO THE MATH
+equals.addEventListener("click", ()=>{
+    if(num.includes("/0")){
+        alert("U dumb or what?");
+        num = num.substring(0, num.length - 1);
+    }
+    sumMade = num.split(separator)
+    let finalResult = operate(sumMade[0], separator, sumMade[1]); 
+    screen.textContent = finalResult;
+    finalResult = num;
+    console.clear();
+});
+
+
+function operate(numb1, oper, numb2){
+    let result;
+    numb1 = Number(numb1);
+    numb2 = Number(numb2);
+    switch(oper){
+        case("+"):
+        result = numb1 + numb2;
+        break;
+
+        case("-"):
+        result = numb1 - numb2;
+        break;
+
+        case("/"):
+        result = numb1 / numb2;
+        break;
+
+        case("*"):
+        result = numb1 * numb2;
+        break;
+    }
+    return result
+}
+
--- Week4/round5Script.js
+++ Week4/round5Script.js
@@ -1,37 +1,3 @@
-
-/* ASSIGNMENT 2.1 */
-function sqrHyp(num1, num2) {
-    let calc = Math.sqrt(num1**2 + num2**2);
-    return calc;
-}
-console.log(sqrHyp(9, 9));
-
-/* ASSIGNMENT 2.2 */
-function triangleArea(side1, side2, side3){
-    let s = (side1 + side2 + side3) / 2 ; 
-    let area = Math.sqrt(s * ((s - side1) * (s - side2) * (s - side3)));
-    return area;
-}
-console.log(triangleArea(5, 6, 7))
-
-/* ASSIGNMET 2.3 */
-function circleDiam(rad){return 2 * Math.PI * rad}
-console.log(circleDiam(4))
-
-
-
-
-
-
-
-/* ASSIGNMENT 3.1 */
-function bigger(a, b) { a > b ? console.log("a") : console.log("b") }
-//bigger(10,9.01)
-
-/* ASSIGNMENT 3.2 */
-function pair(csa) {csa % 2 ? console.log("odd") : console.log("even") }
-//pair(2)
-
 /* Main Assignment */
 function rpc(playerSelection, computerSelection) {
     //get elements from html
@@ -43,8 +9,6 @@ function rpc(playerSelection, computerSelection) {
     let playerScoreHolder = document.querySelector(".playerScore");
     let computerScoreHolder = document.querySelector(".computerScore");
     let computerChoice = document.querySelector(".computerChoice");
-
-
 
     let playerScore = 0;
     let computerScore = 0;
@@ -116,69 +80,22 @@ function rpc(playerSelection, computerSelection) {
                 }
             }
 
+            if(playerScore >= 5){
+                alert("The winner is the Player!")
+                playerScore = 0;
+                computerScore = 0;
+                computerScoreHolder.textContent = 0; 
+                playerScoreHolder.textContent = 0;
+            } else if(computerScore >= 5){
+                alert("The winner is the Computer!")
+                playerScore = 0;
+                computerScore = 0;
+                computerScoreHolder.textContent = 0; 
+                playerScoreHolder.textContent = 0;
+            }
         })
     })
 }
 
 
 rpc();
-
-
-
-function playRound(playerSelect, computerSelect) {
-    playerSelect = window.prompt("Enter your play");
-    playerSelect = playerSelect.toLowerCase();
-    const options = ["rock", "paper", "scissors"];
-    play = Math.floor(Math.random() * options.length);
-    computerSelect = options[play];
-    let winner = "player";
-
-    console.log(playerSelect, computerSelect)
-
-    if (playerSelect == computerSelect) {
-        winner = "tie";
-    }
-
-    else if ((playerSelect == "rock" && computerSelect == "paper") || 
-        (playerSelect == "paper" && computerSelect == "scissors") || 
-        (playerSelect == "scissors" && computerSelect == "rock"))
-    {    
-        winner = "computer";
-    }
-
-    return winner
-}
-
-function game() {
-    let computerPoints = 0;
-    let personPoints = 0;
-
-    for (let i = 0; i < 5; i++) {
-        switch (playRound()) {
-            case ("computer"):
-                console.log(`Computer Wins Round ${i + 1}`)
-                computerPoints++;
-                break;
-
-            case ("player"):
-                console.log(`Player Wins Round ${i + 1}`);
-                personPoints++;
-                break;
-
-            case ("tie"):
-                console.log("It's Tie Round!")
-                break;
-        }
-        console.log("Computer Play:" + computerPoints,"User Play:" + personPoints)
-    }
-
-    if(computerPoints > personPoints){ 
-        return "The Computer is the winner of the game... Better luck next time."; 
-    } else if(computerPoints == personPoints){
-        return "It's a Tie! Try again." 
-    } else{
-        return "You beat the computer! Well Done." 
-    }
-}
-
-console.log(game())
